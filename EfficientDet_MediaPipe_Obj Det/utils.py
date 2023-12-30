dd151fc223d9a428c3b66f9b406373f991a0cded
Sanjana Asrani <109857762+AsraniSanjana@users.noreply.github.com>
2023-12-30 06:51:12 +0530

    Update utils.py

--- EfficientDet_MediaPipe_Obj Det/utils.py
+++ EfficientDet_MediaPipe_Obj Det/utils.py
@@ -1,21 +1,7 @@
-# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Utility functions to display the pose detection results."""
-
 import cv2
 import numpy as np
 from tracker import*
+import cvzone
 
 _MARGIN = 10  # pixels
 _ROW_SIZE = 10  # pixels
@@ -23,10 +9,13 @@ _FONT_SIZE = 1
 _FONT_THICKNESS = 2
 _TEXT_COLOR = (255, 0, 0)  # red
 tracker=Tracker()
-area=[(236,164),(251,183),(275,178),(258,165)]
-area1_c=set()
-area2=[(303,221),(315,248),(340,234),(325,209)]
-area2_c=set()
+# area=[(236,164),(251,183),(275,178), (258,165)]
+area=[(22, 200) , (1270, 200)]   # for upside
+area1_c=set()   # area1: first: bottom to up walking people, lhs polygon
+
+# area2=[(303,221),(315,248),(340,234),(325,209)]   
+area2=[(22, 200) , (1270, 200)]  # for downside
+area2_c=set()   # area2: second: up to down walking people, rhs polygon
 def visualize(
     image: np.ndarray,
     detection_result,
@@ -50,35 +39,44 @@ def visualize(
     x1,y1=end_point
     list.append([x,y,x1,y1])
     
-#    cv2.rectangle(image, (x,y),(x1,y1), (0,0,255), 3)  
+  
     # Draw label and score
     category = detection.categories[0]
     category_name = category.category_name
+
+    # if 'person' in category_name: 
+    #   list.append([x,y,x1,y1])
+    #   cv2.rectangle(image, (x,y),(x1,y1), (0,0,255), 3) 
+    # print(category_name) #class label objname
     probability = round(category.score, 2)
     result_text = category_name + ' (' + str(probability) + ')'
-    text_location = (_MARGIN + bbox.origin_x,
-                     _MARGIN + _ROW_SIZE + bbox.origin_y)
+    text_location = (_MARGIN + bbox.origin_x, _MARGIN + _ROW_SIZE + bbox.origin_y)
   bbox_idx=tracker.update(list)
   for bbox in bbox_idx:
       x2,y2,x3,y3,id=bbox
       cx=int(x2+x3)//2
       cy=int(y2+y3)//2
-      results1=cv2.pointPolygonTest(np.array(area,np.int32),((cx,cy)),False)
+      results1=cv2.pointPolygonTest(np.array(area,np.int32),((cx,cy)),False)   #centroid for lhs area
       if results1>=0:
          cv2.circle(image,(cx,cy),4,(255,0,255),-1)
          cv2.rectangle(image,(x2,y2),(x3,y3),(0,255,0),2)
          cv2.putText(image, str(id), (x2,y2), cv2.FONT_HERSHEY_PLAIN,
-                _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
-         area1_c.add(id)
-      results=cv2.pointPolygonTest(np.array(area2,np.int32),((x3,y3)),False)
+                _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS),area1_c.add(id)
+      results=cv2.pointPolygonTest(np.array(area2,np.int32),((x3,y3)),False)   #bottom right for rhs
       if results>=0:
          cv2.circle(image,(x3,y3),4,(255,0,255),-1)
          cv2.rectangle(image,(x2,y2),(x3,y3),(0,255,0),2)
-         cv2.putText(image, str(id), (x2,y2), cv2.FONT_HERSHEY_PLAIN,
-                _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
+         cv2.putText(image, str(id), (x2,y2), cv2.FONT_HERSHEY_PLAIN, _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
          area2_c.add(id)
-  print(area2_c)
-  print(area1_c)
+  a=(len(area2_c))
+  b=(len(area1_c))
+  print("# people moving down: "+str(a))
+  print("# people moving up: "+str(b))
+
+  cvzone.putTextRect(image, f'moving Up: {b}', (20, 174), 1, 3, (255, 255, 255), 2, cv2.LINE_AA)  # White text
+  cvzone.putTextRect(image, f'moving Down: {a}', (20, 250), 1, 3, (255, 255, 255), 2, cv2.LINE_AA)  # White text
+
+
   cv2.polylines(image,[np.array(area,np.int32)],True,(255,0,0),2)
 
   cv2.polylines(image,[np.array(area2,np.int32)],True,(255,0,0),2)
